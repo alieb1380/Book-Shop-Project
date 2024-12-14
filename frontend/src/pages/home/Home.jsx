98d84977d032fc6c8bf49c63f298e81e5fadf167
Ali Ebrahimi <alieb1380alieb@gmail.com>
2024-12-14 23:01:49 +0330

    End of The TopSellers Section

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Banner";
+import TopSellers from "./TopSellers";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <TopSellers />
+    </>
+  );
+};
+
+export default Home;
